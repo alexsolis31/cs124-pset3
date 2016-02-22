65db49e2336a448e4d53e4a690dc7a41dc4e7764
alexsolis31 <solis@college.harvard.edu>
2016-02-22 12:52:01 -0500

    primstest

--- primsTest.c
+++ primsTest.c
@@ -17,10 +17,10 @@ unsigned nodes1 = 5;
 completeGraph *testGraph1 = genCompleteGraph(nodes1);
 
 vertex a = {0, NULL, INFINITY, 0, 0, 0, 0, 0};
-vertex b = {0, NULL, INFINITY, 0, 0, 0, 0, 0};
-vertex c = {0, NULL, INFINITY, 0, 0, 0, 0, 0};
-vertex d = {0, NULL, INFINITY, 0, 0, 0, 0, 0};
-vertex e = {0, NULL, INFINITY, 0, 0, 0, 0, 0};
+vertex b = {1, NULL, INFINITY, 0, 0, 0, 0, 0};
+vertex c = {2, NULL, INFINITY, 0, 0, 0, 0, 0};
+vertex d = {3, NULL, INFINITY, 0, 0, 0, 0, 0};
+vertex e = {4, NULL, INFINITY, 0, 0, 0, 0, 0};
 
 
 float edgeList1[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
@@ -34,8 +34,7 @@ testGraph1->vertexList[2] = c;
 testGraph1->vertexList[3] = d; 
 testGraph1->vertexList[4] = e; 
 
-testGraph1->numNodes = 5; 
-
+testGraph1->numNodes = 5;
 
 float weight1 = findMST_Weight(testGraph1);
 
@@ -65,13 +64,13 @@ unsigned nodes3 = 5;
 completeGraph *testGraph3 = genCompleteGraph(nodes3);
 
 vertex g = {0, NULL, INFINITY, 0, 0, 0, 0, 0};
-vertex h = {0, NULL, INFINITY, 0, 0, 0, 0, 0};
-vertex i = {0, NULL, INFINITY, 0, 0, 0, 0, 0};
-vertex j = {0, NULL, INFINITY, 0, 0, 0, 0, 0};
-vertex k = {0, NULL, INFINITY, 0, 0, 0, 0, 0};
+vertex h = {1, NULL, INFINITY, 0, 0, 0, 0, 0};
+vertex i = {2, NULL, INFINITY, 0, 0, 0, 0, 0};
+vertex j = {3, NULL, INFINITY, 0, 0, 0, 0, 0};
+vertex k = {4, NULL, INFINITY, 0, 0, 0, 0, 0};
 
 
-float edgeList3[10] = {1, 10, 2, 3, 5, 5, 7, 8, 2, 3};
+float edgeList3[10] = {100, 100, 100, 100, 100, 100, 100, 100, 100, 100};
 
 
 testGraph3->edges = edgeList3; 
@@ -88,7 +87,37 @@ testGraph3->numNodes = 5;
 float weight3 = findMST_Weight(testGraph3);
 
 
-printf("The weight of the Test Graph: %f\n", weight3);
+// printf("The weight of the Test Graph: %f\n", weight3);
+
+// **************************************************************
+// case 4: some edges are the same 
+// **************************************************************
+unsigned nodes4 = 4; 
+completeGraph *testGraph4 = genCompleteGraph(nodes4);
+
+vertex l = {1, NULL, INFINITY, 0, 0, 0, 0, 0};
+vertex m = {2, NULL, INFINITY, 0, 0, 0, 0, 0};
+vertex n = {3, NULL, INFINITY, 0, 0, 0, 0, 0};
+vertex o = {4, NULL, INFINITY, 0, 0, 0, 0, 0};
+
+float edgeList4[6] = {3, 3, 10, 100, 3, 2};
+
+
+testGraph4->edges = edgeList4; 
+
+testGraph4->vertexList[0] = l; 
+testGraph4->vertexList[1] = m; 
+testGraph4->vertexList[2] = n; 
+testGraph4->vertexList[3] = o; 
+
+
+testGraph4->numNodes = 5; 
+
+
+float weight4 = findMST_Weight(testGraph4);
+
+
+printf("The weight of the Test Graph: %f\n", weight4);
 
 
 }
